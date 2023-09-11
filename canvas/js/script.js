f4790929d3484f1a2f24045cca0c8fc79e51461a
Cauã Magalhães Leão <127852558+MrMagalhaes@users.noreply.github.com>
2023-09-10 22:34:53 -0300

    Add files via upload

--- canvas/js/script.js
+++ canvas/js/script.js
@@ -1,91 +1,99 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
-const jogador = {
+const musicaFundo = new Audio('audio/trem.mp3');
 
-    x:50,
-    y:canvas.heigth - 50,
+// Reproduzir a música de fundo
+musicaFundo.play();
+
+const jogador = {
+    x: 50,
+    y: canvas.height - 50,
     width: 30,
-    height:30,
-    velocityY:0,
-    gravity:1.5,
-    isJumping:false,
+    height: 30,
+    velocityY: 1.0,
+    gravity: 1.5,
+    isJumping: false,
     jumpHeight: -20,
 };
 
-const obstaculos =[];
-const obstaculosWidth = 20;
-const obstaculosHeight = 20;
+const obstaculos = [];
+const obstaculosWidth = 5;
+const obstaculosHeight = 5;
 const obstaculosVelocityX = -5;
 
 let pontuacao = 0;
 
 function desenharJogador() {
-
-    ctx.fillStyle = 'blue';
+    // Cabeça
+    ctx.fillStyle = 'black';
+    ctx.beginPath();
+    ctx.arc(jogador.x + jogador.width / 2, jogador.y - 20, 10, 0, Math.PI * 2);
+    ctx.fill();
+    
+    // Corpo
     ctx.fillRect(jogador.x, jogador.y, jogador.width, jogador.height);
 
+    // Pernas
+    ctx.fillRect(jogador.x, jogador.y + jogador.height, 10, 20);
+    ctx.fillRect(jogador.x + jogador.width - 10, jogador.y + jogador.height, 10, 20);
 }
 
-function desenharObstaculos(){
 
-    ctx.fillStyle = 'red';
-    for (const obstaculo of obstaculos){
-        ctx.fillRect(obstaculo.x, obstaculo.y, obstaculoWidth, obstaculoHeight);
+function desenharObstaculos() {
+    for (const obstaculo of obstaculos) {
+        ctx.fillRect(obstaculo.x, obstaculo.y, obstaculosWidth, obstaculosHeight);
     }
 }
 
-function atualizarJogo(){
-
+function atualizarJogo() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-
-    if(jogador.isJumping){
+    if (jogador.isJumping) {
         jogador.velocityY += jogador.gravity;
         jogador.y += jogador.velocityY;
-        if (jogador.y > canvas.height - jogador.heigth){
-            jogador.y = canvas.heigth - jogador.heigth;
+        if (jogador.y > canvas.height - jogador.height) {
+            jogador.y = canvas.height - jogador.height;
             jogador.isJumping = false;
         }
     }
 
-    if (obstaculos.length === 0 || obstaculos[0].x <= canvas.width - 200){
-        criarObstaculos();
+    if (obstaculos.length === 0 || obstaculos[0].x <= canvas.width - 200) {
+        criarObstaculo();
     }
 
-    for (let i = obstaculos.length - 1; i >= 0; i--){
-        obstaculos[i].x += obstaculoVelocityX;
+    for (let i = obstaculos.length - 1; i >= 0; i--) {
+        obstaculos[i].x += obstaculosVelocityX;
 
-        if(obstaculos[i].x + obstaculoWidth < 0){
+        if (obstaculos[i].x + obstaculosWidth < 0) {
             obstaculos.splice(i, 1);
             pontuacao++;
         }
     }
 
-desenharJogador();
-desenharObstaculos();
+    desenharJogador();
+    desenharObstaculos();
 
-ctx.fillStyle = 'black';
-ctx.font = '24px Arial';
-ctx.fillText(`Pontuação: ${pontuacao}`, 10, 30);
+    ctx.fillStyle = 'black';
+    ctx.font = '24px Arial';
+    ctx.fillText(`Pontuação: ${pontuacao}`, 10, 30);
 
-requestAnimationFrame(atualizarJogo);
-
+    requestAnimationFrame(atualizarJogo);
 }
-function criarObstaculo(){
+
+function criarObstaculo() {
     const obstaculo = {
-        x:canvas.width,
-        y:canvas.heigth - obstaculoHeight,
+        x: canvas.width,
+        y: canvas.height - obstaculosHeight,
     };
     obstaculos.push(obstaculo);
 }
 
-document.addEventListener('keydown', function(event){
-    if(event.keyCode === 32 && !jogador.isJumping){
+document.addEventListener('keydown', function (event) {
+    if (event.key === ' ' && !jogador.isJumping) {
         jogador.velocityY = jogador.jumpHeight;
         jogador.isJumping = true;
     }
-
 });
 
 atualizarJogo();
